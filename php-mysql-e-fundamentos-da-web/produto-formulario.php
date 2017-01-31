7441c4ad34368ed19275d14e7df131bcf029b687
Estevam de Moraes Rodrigues <estevam.mrodrigues@gmail.com>
2017-01-31 14:07:32 -0200

    Primeiro capítulo php-mysql

---
+++ php-mysql-e-fundamentos-da-web/produto-formulario.php
@@ -0,0 +1,10 @@
+<?php include("cabecalho.php") ?>
+  <h1>Formulário de cadastro</h1>
+  <form action="adiciona-produto.php">
+    Nome: 
+    <input type="text" name="nome"><br><br>
+    Preço: 
+    <input type="number" name="preco"><br><br>
+    <input class="btn btn-primary" type="submit" value="Cadastrar" />
+  </form>
+<?php include("rodape.php") ?>
