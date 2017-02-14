198ca37daa1ba926943c63ea587fc09748229ba7
Estevam de Moraes Rodrigues <estevam.mrodrigues@gmail.com>
2017-02-14 00:26:25 -0200

    dom e objetos

--- js/alura-js/calcula-imcs.js
+++ js/alura-js/calcula-imcs.js
@@ -1,10 +1,20 @@
-//calcula-imc.js
-var peso = 100;
-var altura = 2.0; //testando com 0
+var peso = document.getElementById("peso-1").textContent; // pega o peso do paciente
+var altura = document.getElementById("altura-1").textContent; // pega a altura do paciente
+var nome = document.getElementById("nome-1").textContent; // pega o nome do paciente
 
-if(altura != 0){
-    var imc = peso / (altura * altura);
+var paciente = {
+    peso : peso,
+    altura : altura,
+    nome : nome
+}
+
+if(paciente.altura != 0){
+    var imc = paciente.peso / (paciente.altura * paciente.altura);
+
+    var tdDoImc = document.getElementById("imc-1"); // pega o td do imc do paciente 1
+    tdDoImc.textContent = imc; //Altera o conteúdo do td para o valor da variável imc
+
     console.log(imc);
-} else{
-    console.log("Não posso executar uma divisão por 0!");
+}else{
+    console("Não posso executar uma divisão por 0!");
 }
