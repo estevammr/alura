1c6334f65d749a9c0baa3a8537f505f4ac8e1547
Estevam de Moraes Rodrigues <estevam.rodrigues@boavistaservicos.com.br>
2017-02-21 14:26:08 -0300

    SQL injection

--- php-mysql-e-fundamentos-da-web/banco-produto.php
+++ php-mysql-e-fundamentos-da-web/banco-produto.php
@@ -10,6 +10,11 @@ function listaProdutos($conexao) {
 }
 
 function insereProduto ($conexao, $nome, $preco, $descricao, $categoria_id, $usado) {
+  $nome = mysqli_real_escape_string($conexao, $nome);
+  $preco = mysqli_real_escape_string($conexao, $preco);
+  $descricao = mysqli_real_escape_string($conexao, $descricao);
+  $categoria_id = mysqli_real_escape_string($conexao, $categoria_id);
+  $usado = mysqli_real_escape_string($conexao, $usado);
   $query = "insert into produtos (nome, preco, descricao, categoria_id, usado) values ('{$nome}', {$preco}, '{$descricao}', {$categoria_id}, {$usado})";
   $resultadoInsere = mysqli_query($conexao, $query);
   return $resultadoInsere;
@@ -28,6 +33,11 @@ function buscaProduto($conexao, $id) {
 }
 
 function alteraProduto($conexao, $id, $nome, $preco, $descricao, $categoria_id, $usado) {
+  $nome = mysqli_real_escape_string($conexao, $nome);
+  $preco = mysqli_real_escape_string($conexao, $preco);
+  $descricao = mysqli_real_escape_string($conexao, $descricao);
+  $categoria_id = mysqli_real_escape_string($conexao, $categoria_id);
+  $usado = mysqli_real_escape_string($conexao, $usado);
     $query = "update produtos set nome = '{$nome}', preco = {$preco}, descricao = '{$descricao}', 
         categoria_id= {$categoria_id}, usado = {$usado} where id = '{$id}'";
     return mysqli_query($conexao, $query);
--- php-mysql-e-fundamentos-da-web/banco-usuario.php
+++ php-mysql-e-fundamentos-da-web/banco-usuario.php
@@ -2,6 +2,8 @@
 
 function buscaUsuario($conexao, $email, $senha) {
     $senhaMd5 = md5($senha);
+    $email = mysqli_real_escape_string($conexao, $email);
+    $senhaMd5 = mysqli_real_escape_string($conexao, $senhaMd5);
     $query = "select * from usuarios where email='{$email}' and senha='{$senhaMd5 }'";
     $resultado = mysqli_query($conexao, $query);
     $usuario = mysqli_fetch_assoc($resultado);
