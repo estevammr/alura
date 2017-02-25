9bc3d5d04a6d3a4d9d5a5d55af8dea9468db3c26
Estevam de Moraes Rodrigues <estevam.mrodrigues@gmail.com>
2017-02-25 20:13:13 -0300

    Curso angular cap 2

---
+++ angularjs/alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,8 @@
+angular.module('alurapic').controller('FotosController', ['$scope', function($scope){
+    
+  $scope.foto = {
+    url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS0H6_8T93B9doxf8I0uUNfuHEjfwFH3FyNt5y_cDHnFA_OEP5x",
+    titulo: "Tigrão da porra"
+  };
+
+}]);
