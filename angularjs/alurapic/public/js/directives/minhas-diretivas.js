bc57a5a8356b8d3e3af10d7675a47e4510bded3b
Estevam de Moraes Rodrigues <estevam.mrodrigues@gmail.com>
2017-02-26 19:16:26 -0300

    Diretivas para componentizar e rotas

--- angularjs/alurapic/public/js/directives/minhas-diretivas.js
+++ angularjs/alurapic/public/js/directives/minhas-diretivas.js
@@ -13,4 +13,19 @@ angular.module('minhasDiretivas', [])
     ddo.templateUrl = 'js/directives/meu-painel.html';        
 
     return ddo;
+  })
+  .directive('minhaFoto', function() {
+
+    var ddo = {};
+
+    ddo.restrict = "AE";
+
+    ddo.scope = {
+      url: '@',
+      titulo: '@'
+    };
+
+    ddo.templateUrl = 'js/directives/minha-foto.html';
+
+    return ddo;
   });
