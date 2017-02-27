b95c76ccbe0c60cb294ed5e45933aaa20cb4019e
Estevam de Moraes Rodrigues <estevam.mrodrigues@gmail.com>
2017-02-26 22:27:43 -0300

    Editar e deletar

--- angularjs/alurapic/public/js/controllers/fotos-controller.js
+++ angularjs/alurapic/public/js/controllers/fotos-controller.js
@@ -2,6 +2,7 @@ angular.module('alurapic').controller('FotosController', ['$scope', '$http', fun
     
   $scope.fotos = [];
   $scope.filtro = '';
+  $scope.mensagem = '';
   
   $http.get('v1/fotos')
   .success(function(fotos) {
@@ -11,4 +12,18 @@ angular.module('alurapic').controller('FotosController', ['$scope', '$http', fun
     console.log(erro);
   });
 
+  $scope.remover = function(foto) {
+    console.log('remover foto:');
+    console.log(foto);
+    $http.delete('v1/fotos/' + foto._id)
+    .success(function() {
+      var indiceFoto = $scope.fotos.indexOf(foto); // obtem o índice da foto que está sendo removida.
+      $scope.fotos.splice(indiceFoto, 1); // remove um elemento da lista de fotos de acordo com o índice da foto removida.
+      $scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso.';
+    })
+    .error(function(erro) {
+      console.log(erro);
+      $scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
+    });
+};
 }]);
