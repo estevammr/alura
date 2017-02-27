d3b3f4a3f2cb662ad7a1b57b81609cf11d4cc5a4
Estevam de Moraes Rodrigues <estevam.mrodrigues@gmail.com>
2017-02-27 00:29:01 -0300

    Mais diretivas, eventos e manipulando o DOM

--- angularjs/alurapic/public/js/directives/minhas-diretivas.js
+++ angularjs/alurapic/public/js/directives/minhas-diretivas.js
@@ -1,4 +1,4 @@
-angular.module('minhasDiretivas', [])
+angular.module('minhasDiretivas', ['meusServicos'])
   .directive('meuPainel', function() {
 
     var ddo = {};
@@ -40,5 +40,29 @@ angular.module('minhasDiretivas', [])
     
     ddo.template = '<button class="btn btn-danger btn-block" ng-click="acao()">{{nome}}</button>';
 
+    return ddo;
+  })
+  .directive('meuFocus', function() {
+    var ddo = {};
+    ddo.restrict = "A";
+    // não tem mais scope
+    ddo.link = function(scope, element) {
+      scope.$on('fotoCadastrada', function() {
+        element[0].focus();
+      });
+    };
+    return ddo;
+  })
+  .directive('meusTitulos', function() {
+    var ddo = {};
+    ddo.restrict = 'E';
+    ddo.template = '<ul><li ng-repeat="titulo in titulos">{{titulo}}</li></ul>';
+    ddo.controller = function($scope, recursoFoto) {
+      recursoFoto.query(function(fotos) {
+        $scope.titulos = fotos.map(function(foto) {
+          return foto.titulo;
+        });    
+      });
+    };
     return ddo;
   });
