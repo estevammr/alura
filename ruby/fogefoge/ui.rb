f9514dc25db321930e6a8ba38785290821eba856
Estevam de Moraes Rodrigues <estevam.mrodrigues@gmail.com>
2017-03-03 23:43:20 -0300

    Cap 1

--- ruby/fogefoge/ui.rb
+++ ruby/fogefoge/ui.rb
@@ -6,3 +6,12 @@ def da_boas_vindas
   puts "Começaremos o jogo para você, #{nome}"
   nome
 end
+
+def pede_movimento
+  puts "Para onde deseja ir?"
+  movimento = gets.strip
+end
+
+def desenha mapa
+  puts mapa
+end
